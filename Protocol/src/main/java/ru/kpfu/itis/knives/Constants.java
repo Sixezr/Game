12330ef640ca1ea8673173cf39925af82e2317c0
Regina Tiapkina <rtapkina@gmail.com>
2021-12-18 20:53:30 +0300

    some new common classes

---
+++ Protocol/src/main/java/ru/kpfu/itis/knives/Constants.java
@@ -0,0 +1,11 @@
+package ru.kpfu.itis.knives;
+
+public class Constants {
+    public static final int MAX_PLAYER_NUM = 2;
+
+    public static final int START_MAX_X = 100;
+    public static final int START_MAX_Y = 100;
+    public static final int START_MIN_Y = -100;
+    public static final int START_MIN_X = -100;
+    public static final int START_0 = 0;
+}
