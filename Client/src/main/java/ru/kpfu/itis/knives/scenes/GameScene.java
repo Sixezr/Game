9b54be344ac8455d77f76d1b342ea1e7d36c9f1a
Snitsaryuk Roman <71031730+Sixezr@users.noreply.github.com>
2021-12-18 22:24:50 +0300

    added game scene

---
+++ Client/src/main/java/ru/kpfu/itis/knives/scenes/GameScene.java
@@ -0,0 +1,39 @@
+package ru.kpfu.itis.knives.scenes;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import ru.kpfu.itis.knives.App;
+import ru.kpfu.itis.knives.view.HeadMenuBar;
+import ru.kpfu.itis.knives.view.ProgressHBox;
+
+public class GameScene extends Scene {
+    // UI
+    private final BorderPane mainPane;
+    private final ProgressHBox progressHBox;
+    private final Label statusLabel;
+
+    // Init
+    public GameScene(BorderPane pane) {
+        super(pane, App.getMainStage().getWidth(), App.getMainStage().getHeight());
+        mainPane = pane;
+        mainPane.setTop(new HeadMenuBar());
+        progressHBox = new ProgressHBox("Игра началась");
+        mainPane.setBottom(progressHBox);
+
+        VBox contentVBox = new VBox();
+        contentVBox.setSpacing(64);
+        contentVBox.setAlignment(Pos.CENTER);
+
+        statusLabel = new Label("Игра началась");
+
+        // TODO: add game circle to HBox and to right add messages
+        HBox contentHBox = new HBox();
+
+        contentVBox.getChildren().addAll(statusLabel);
+        mainPane.setCenter(contentVBox);
+    }
+}
