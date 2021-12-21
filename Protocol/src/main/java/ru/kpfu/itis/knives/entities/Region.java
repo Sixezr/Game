975cb29df312e6c4bc72560cc1bead888fc9f4c5
GalyautdinovIslam <galyautdinovislam@gmail.com>
2021-12-21 10:58:47 +0300

    Updated: - Region class (union and divide methods) - Bound class

--- Protocol/src/main/java/ru/kpfu/itis/knives/entities/Region.java
+++ Protocol/src/main/java/ru/kpfu/itis/knives/entities/Region.java
@@ -24,9 +24,7 @@ public class Region {
         bounds.remove(bound);
 
         this.bounds = connect(bounds, bound);
-        calcSquare();
-        calcIsland();
-        calcXBounds();
+        calcAll();
     }
 
     private List<Bound> connect(List<Bound> bounds, Bound start) throws WrongRegionBoundsException {
@@ -45,6 +43,7 @@ public class Region {
                 connected.setConnected(true);
                 first = bound.getPoints().get(0).equals(first) ? bound.getPoints().get(1) : bound.getPoints().get(0);
                 result.add(bound);
+                bounds.remove(bound);
             } else {
                 throw new WrongRegionBoundsException("Нет следующей для соединения границы.");
             }
@@ -70,6 +69,130 @@ public class Region {
             if (bound.getPoints().contains(point) &&
                     (bound.getPoints().get(0).equals(point) && !bound.getPoints().get(0).isConnected() ||
                             bound.getPoints().get(1).equals(point) && !bound.getPoints().get(1).isConnected())) {
+                return Optional.of(bound);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public void union(Region region) {
+        List<Bound> result = new ArrayList<>();
+        for (int i = this.bounds.size() - 1; i >= 0; i--) {
+            for (int j = 0; j < region.bounds.size(); j++) {
+                if (this.bounds.get(i).isLine() == region.bounds.get(j).isLine() &&
+                        this.bounds.get(i).getQuarter() == region.bounds.get(j).getQuarter() &&
+                        Math.abs(this.bounds.get(i).getK() - region.bounds.get(j).getK()) <= 1e-3 &&
+                        Math.abs(this.bounds.get(i).getB() - region.bounds.get(j).getB()) <= 1e-3 &&
+                        (this.bounds.get(i).getPoints().get(0).getX() <= region.getBounds().get(j).getPoints().get(0).getX() &&
+                                region.getBounds().get(j).getPoints().get(0).getX() <= this.bounds.get(i).getPoints().get(1).getX() ||
+                                region.getBounds().get(j).getPoints().get(0).getX() <= this.bounds.get(i).getPoints().get(0).getX() &&
+                                        this.bounds.get(i).getPoints().get(0).getX() <= region.getBounds().get(j).getPoints().get(1).getX())) {
+
+                    if (this.bounds.get(i).getPoints().get(0).getX() <= region.getBounds().get(j).getPoints().get(0).getX() &&
+                            this.bounds.get(i).getPoints().get(1).getX() >= region.getBounds().get(j).getPoints().get(1).getX()) {
+                        result.add(region.getBounds().get(j));
+                    } else {
+                        result.add(this.bounds.get(i));
+                    }
+                    region.getBounds().remove(region.getBounds().get(j));
+                    this.bounds.remove(this.bounds.get(i));
+                    break;
+                }
+            }
+        }
+
+        this.bounds = result;
+        calcAll();
+    }
+
+    public List<Region> divide(Bound bound) {
+        List<Region> result = new ArrayList<>();
+        List<Bound> first = new ArrayList<>();
+        List<Bound> second = new ArrayList<>();
+
+        List<Point> points = new ArrayList<>();
+
+        List<Bound> commons = new ArrayList<>();
+
+        for (Bound b : this.bounds) {
+            if (b.samePoint(bound).isPresent()) {
+                points.add(b.samePoint(bound).get());
+            }
+        }
+
+        points.sort((o1, o2) ->
+                (int) (Math.sqrt((o1.getX() - o2.getX()) * (o1.getX() - o2.getX()) +
+                        (o1.getY() - o2.getY()) * (o1.getY() - o2.getY())) * 1e3));
+
+        Point firstPoint = points.get(0);
+        Point secondPoint = null;
+
+        for (Point p : bound.getPoints()) {
+            if (this.hasPoint(p)) {
+                for (int i = 1; i < points.size(); i++) {
+                    if (p.getX() >= firstPoint.getX() && points.get(i).getX() >= p.getX() ||
+                            p.getX() <= firstPoint.getX() && points.get(i).getX() <= p.getX()) {
+                        secondPoint = points.get(i);
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+
+        for (Bound b : this.bounds) {
+            if (b.contains(firstPoint) || b.contains(Objects.requireNonNull(secondPoint))) {
+                commons.add(b);
+            }
+        }
+
+        bound = new Bound(true, firstPoint, secondPoint);
+
+        first.add(bound);
+        second.add(bound);
+
+        List<Bound> bounds = this.bounds;
+        if (commons.size() < 4) {
+            for (int i = commons.size() - 1; i >= 0; i--) {
+                bounds.remove(commons.get(i));
+                if (!commons.get(i).getPoints().contains(firstPoint) && !commons.get(i).getPoints().contains(secondPoint)) {
+                    if (commons.get(i).contains(firstPoint)) {
+                        commons.add(new Bound(commons.get(i).isLine(), commons.get(i).getPoints().get(0), firstPoint));
+                        commons.add(new Bound(commons.get(i).isLine(), commons.get(i).getPoints().get(1), firstPoint));
+                    } else {
+                        commons.add(new Bound(commons.get(i).isLine(), commons.get(i).getPoints().get(0), secondPoint));
+                        commons.add(new Bound(commons.get(i).isLine(), commons.get(i).getPoints().get(1), secondPoint));
+                    }
+                    commons.remove(i);
+                }
+            }
+            bounds.addAll(commons);
+        }
+
+        while (!firstPoint.equals(secondPoint) && findNextWithoutConnection(bounds, firstPoint).isPresent()) {
+            Bound toAdd = findNextWithoutConnection(bounds, firstPoint).get();
+            firstPoint = toAdd.getPoints().get(0).equals(firstPoint) ?
+                    toAdd.getPoints().get(1) :
+                    toAdd.getPoints().get(0);
+
+            first.add(toAdd);
+            bounds.remove(toAdd);
+        }
+        second.addAll(bounds);
+
+        try {
+            result.add(new Region(this.owner, first));
+            result.add(new Region(this.owner, second));
+        } catch (WrongRegionBoundsException e) {
+            // TODO: обработать исключение
+        }
+
+        return result;
+    }
+
+    private Optional<Bound> findNextWithoutConnection(List<Bound> bounds, Point point) {
+        for (Bound bound : bounds) {
+            if (bound.getPoints().contains(point)) {
                 return Optional.of(bound);
             }
         }
@@ -121,6 +244,12 @@ public class Region {
         this.square = square;
     }
 
+    public void calcAll() {
+        calcSquare();
+        calcIsland();
+        calcXBounds();
+    }
+
     private Pair getPair(Pair thisPair, List<Pair> lastPairs) {
         Pair result = null;
         float size = Float.MIN_VALUE;
@@ -134,7 +263,7 @@ public class Region {
                 result = pair;
             }
         }
-        if (result == null) result = new Pair(0, 0);
+        if (result == null) result = new Pair(thisPair.getSize() / 2, thisPair.getSize() / 2);
         return result;
     }
 
