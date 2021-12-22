a9a1ff5c715183d6020993f5b35baf101cd617ff
Elina Kadyrova <kad_elina@mail.ru>
2021-12-22 20:54:03 +0300

    Add ServerGameController methods.

--- Server/src/main/java/ru/kpfu/itis/knives/game/ServerGameController.java
+++ Server/src/main/java/ru/kpfu/itis/knives/game/ServerGameController.java
@@ -1,7 +1,6 @@
 package ru.kpfu.itis.knives.game;
 
-import ru.kpfu.itis.knives.entities.AbstractGameController;
-import ru.kpfu.itis.knives.entities.GameSession;
+import ru.kpfu.itis.knives.entities.*;
 
 import java.util.Random;
 
@@ -13,5 +12,40 @@ public class ServerGameController extends AbstractGameController implements Serv
     @Override
     public int getRandomPlayerId() {
         return (new Random().nextInt());
+    }
+
+    @Override
+    public Player getCurrentPlayer() {
+        return null;
+    }
+
+    @Override
+    public float getRandomKnifeFallAngle() {
+        return 0;
+    }
+
+    @Override
+    public boolean checkPointIsInCircle(Point point) {
+        return false;
+    }
+
+    @Override
+    public boolean checkPointBelongsToPlayerRegion(Point point, Player player) {
+        return false;
+    }
+
+    @Override
+    public boolean checkRegionIsIsland(Region region) {
+        return false;
+    }
+
+    @Override
+    public boolean isPlayerHasEnoughTerritory(Player player) {
+        return false;
+    }
+
+    @Override
+    public boolean checkPointBelongsToRegion(Point point, Region region) {
+        return false;
     }
 }
--- Server/src/main/java/ru/kpfu/itis/knives/game/ServerGameControllerInterface.java
+++ Server/src/main/java/ru/kpfu/itis/knives/game/ServerGameControllerInterface.java
@@ -1,6 +1,16 @@
 package ru.kpfu.itis.knives.game;
 
+import ru.kpfu.itis.knives.entities.Player;
+import ru.kpfu.itis.knives.entities.Point;
+import ru.kpfu.itis.knives.entities.Region;
+
 public interface ServerGameControllerInterface {
     int getRandomPlayerId();
-    // todo
+    Player getCurrentPlayer();
+    float getRandomKnifeFallAngle();
+    boolean checkPointIsInCircle(Point point);
+    boolean checkPointBelongsToPlayerRegion(Point point, Player player);
+    boolean checkRegionIsIsland(Region region);
+    boolean isPlayerHasEnoughTerritory(Player player);
+    boolean checkPointBelongsToRegion(Point point, Region region);
 }
