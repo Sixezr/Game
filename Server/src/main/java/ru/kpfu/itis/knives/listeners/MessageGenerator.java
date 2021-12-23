2713d6e18d4904021a3930613c9d083d1bff3152
Regina Tiapkina <rtapkina@gmail.com>
2021-12-23 13:33:43 +0300

    message generator : protocol changes, message data changes

--- Server/src/main/java/ru/kpfu/itis/knives/listeners/MessageGenerator.java
+++ Server/src/main/java/ru/kpfu/itis/knives/listeners/MessageGenerator.java
@@ -38,11 +38,28 @@ public class MessageGenerator implements MessageGeneratorInterface {
         Message newMessage = new Message(type);
         switch (type) {
             case MOVE_RESULT_GOOD:
+                if (ids.length != 1 && values.length != 5) {
+                    throw new MessageGenerationException("Message of this type must contain 2 float value and 1 integer values");
+                } else {
+                    newMessage.setData(ByteBuffer.allocate(24).putFloat(values[0])
+                            .putInt(ids[0])
+                            .putFloat(values[1]).putFloat(values[2])
+                            .putFloat(values[3]).putFloat(values[4])
+                            .array());
+                }
+                break;
             case MOVE_RESULT_BAD:
                 if (ids.length != 1 && values.length != 1) {
                     throw new MessageGenerationException("Message of this type must contain 1 float value and 1 integer values");
                 } else {
                     newMessage.setData(ByteBuffer.allocate(8).putFloat(values[0]).putInt(ids[0]).array());
+                }
+                break;
+            case MOVE_RESULT:
+                if (ids.length != 1 && values.length != 2) {
+                    throw new MessageGenerationException("Message of this type must contain 2 float value and 1 integer values");
+                } else {
+                    newMessage.setData(ByteBuffer.allocate(12).putInt(ids[0]).putFloat(values[0]).putFloat(values[1]).array());
                 }
                 break;
             default:
@@ -62,7 +79,8 @@ public class MessageGenerator implements MessageGeneratorInterface {
                     newMessage.setData(ByteBuffer.allocate(8).putInt(ids[0]).putInt(ids[1]).array());
                 }
                 break;
-            case MOVE_RESULT:
+            case CLIENT_LEFT:
+            case GAME_INTERRUPT:
             case GAME_END:
                 if (ids.length != 1) {
                     throw new MessageGenerationException("Message must contain 1 int value");
