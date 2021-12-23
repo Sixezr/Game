1c50674126f0b396fae8a309f24172fe155b7826
Snitsaryuk Roman <71031730+Sixezr@users.noreply.github.com>
2021-12-23 19:19:19 +0300

    add action for home

--- Client/src/main/java/ru/kpfu/itis/knives/controllers/WaitingController.java
+++ Client/src/main/java/ru/kpfu/itis/knives/controllers/WaitingController.java
@@ -11,12 +11,14 @@ public final class WaitingController extends AbstractController {
     // UI
     private final BorderPane mainPane;
     private final WaitingVBox waitingVBox;
+    private final HeadMenuBar headMenuBar;
 
     // Init
     public WaitingController(Stage stage) {
         super(stage);
         mainPane = new BorderPane();
         waitingVBox = new WaitingVBox();
+        headMenuBar = new HeadMenuBar();
 
         stage.setMinWidth(600);
         stage.setMinHeight(400);
@@ -26,7 +28,7 @@ public final class WaitingController extends AbstractController {
     // Create scene
     @Override
     public void createScene() {
-        mainPane.setTop(new HeadMenuBar());
+        mainPane.setTop(headMenuBar);
         mainPane.setCenter(waitingVBox);
         mainPane.setBottom(new ProgressHBox("Поиск соперника..."));
 
@@ -37,6 +39,12 @@ public final class WaitingController extends AbstractController {
     // Private
     private void addActions() {
         waitingVBox.getCancelButton().setOnAction(event -> {
+            // TODO: Network actions
+            AbstractController initialController = new InitialController(stage);
+            initialController.createScene();
+        });
+
+        headMenuBar.getMainItemLabel().setOnMouseClicked(event -> {
             // TODO: Network actions
             AbstractController initialController = new InitialController(stage);
             initialController.createScene();
