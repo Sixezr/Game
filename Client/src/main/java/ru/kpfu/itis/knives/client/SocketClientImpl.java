965a53551c0dd57705b6f03f5e8bb39efb91e9c3
GalyautdinovIslam <galyautdinovislam@gmail.com>
2021-12-24 00:54:34 +0300

    пушед сокет клиент импл)

--- Client/src/main/java/ru/kpfu/itis/knives/client/SocketClientImpl.java
+++ Client/src/main/java/ru/kpfu/itis/knives/client/SocketClientImpl.java
@@ -1,8 +1,10 @@
 package ru.kpfu.itis.knives.client;
 
+import ru.kpfu.itis.knives.controllers.AbstractController;
 import ru.kpfu.itis.knives.entities.GameControllerInterface;
 import ru.kpfu.itis.knives.entities.GameSession;
 import ru.kpfu.itis.knives.entities.Player;
+import ru.kpfu.itis.knives.entities.Point;
 import ru.kpfu.itis.knives.generators.MessageGenerator;
 import ru.kpfu.itis.knives.generators.MessageGeneratorImpl;
 import ru.kpfu.itis.knives.listeners.ClientMessageListener;
@@ -11,6 +13,7 @@ import ru.kpfu.itis.knives.protocol.Message;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SocketClientImpl implements SocketClient {
@@ -18,8 +21,9 @@ public class SocketClientImpl implements SocketClient {
     private GameControllerInterface regionsController;
     private MessageGenerator messageGenerator;
     private Player player;
+    private AbstractController controller;
 
-    private List<ClientMessageListener> listeners;
+    private List<ClientMessageListener> listeners = new ArrayList<>();
 
     public SocketClientImpl(InetAddress host, int port) {
         try {
@@ -62,7 +66,7 @@ public class SocketClientImpl implements SocketClient {
     }
 
     @Override
-    public void move() {
+    public void move(Point from, Point to) {
 
     }
 
@@ -87,12 +91,7 @@ public class SocketClientImpl implements SocketClient {
     }
 
     @Override
-    public void choiceRegion() {
-
-    }
-
-    @Override
-    public void repeat() {
+    public void choiceRegion(Point point) {
 
     }
 
@@ -109,5 +108,53 @@ public class SocketClientImpl implements SocketClient {
     @Override
     public void sendMessage(Message message) {
         connection.sendMessage(message);
+    }
+
+    public ClientMessagesHandler getConnection() {
+        return connection;
+    }
+
+    public void setConnection(ClientMessagesHandler connection) {
+        this.connection = connection;
+    }
+
+    public GameControllerInterface getRegionsController() {
+        return regionsController;
+    }
+
+    public void setRegionsController(GameControllerInterface regionsController) {
+        this.regionsController = regionsController;
+    }
+
+    public MessageGenerator getMessageGenerator() {
+        return messageGenerator;
+    }
+
+    public void setMessageGenerator(MessageGenerator messageGenerator) {
+        this.messageGenerator = messageGenerator;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public AbstractController getController() {
+        return controller;
+    }
+
+    public void setController(AbstractController controller) {
+        this.controller = controller;
+    }
+
+    public List<ClientMessageListener> getListeners() {
+        return listeners;
+    }
+
+    public void setListeners(List<ClientMessageListener> listeners) {
+        this.listeners = listeners;
     }
 }
